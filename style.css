6969bab1db3636ed33174791d549f2d268483544
adityanarayansinghraghuvanshi-tech <adityanarayansinghraghuvanshi@gmail.com>
2026-07-02 12:08:12 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,55 @@
+body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+    background-color: #0f0f0f;
+    color: white;
+}
+
+/* HEADER */
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    background: #1a1a1a;
+}
+
+header h1 {
+    margin: 0;
+    color: gold;
+}
+
+nav a {
+    color: white;
+    margin: 0 10px;
+    text-decoration: none;
+}
+
+nav a:hover {
+    color: gold;
+}
+
+/* HERO */
+.hero {
+    text-align: center;
+    padding: 100px 20px;
+    background: linear-gradient(to right, #111, #222);
+}
+
+.hero h2 {
+    color: gold;
+    font-size: 32px;
+}
+
+/* SECTIONS */
+section {
+    padding: 40px;
+}
+
+/* FOOTER */
+footer {
+    text-align: center;
+    padding: 20px;
+    background: #1a1a1a;
+    color: gray;
+}
